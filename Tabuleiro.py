77959e4dd8938788f2d05eafb2704a87cb48c205
José Rodrigues <joserodriguesrs18@gmail.com>
2018-05-02 20:20:35 -0700

    update

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -3,13 +3,14 @@
 class tabuleiro():
     
     def __init__(self):
-        
+    
         self.token = "X"
         self.jogo = [[[] for i in range(3)] for i in range(3)]
         self.linha = None
         self.coluna = None
 
     def linha1(self):
+
         self.coluna = input("Qual a posição nessa linha, A, B ou C:") 
         if self.token not in self.jogo[0][0]:
             if self.coluna == "A":  
@@ -20,8 +21,7 @@ class tabuleiro():
         if self.token not in self.jogo[0][2]:
             if self.coluna == "C":
                 self.jogo[0][2].append(self.token)
-        else:
-            print("teste")
+                
         
         for listas in self.jogo:
             print()
@@ -30,6 +30,7 @@ class tabuleiro():
        
         
     def linha2(self):
+
         self.coluna = input("Qual a posição nessa linha, A, B ou C:")
         if self.token not in self.jogo[1][0]:
             if self.coluna == "A":
@@ -49,6 +50,7 @@ class tabuleiro():
                       
 
     def linha3(self):
+
         self.coluna = input("Qual a posição nessa linha, A, B ou C:")
         if self.token not in self.jogo[2][0]:
             if self.coluna == "A":
@@ -75,6 +77,7 @@ while rounds != 0:
     print("A jogar: Jogador 1")
     jogada.linha = int(input("Qual a linha em que queres jogar 1, 2, 3:"))
     rounds -= 3
+    
     if jogada.linha == 1:
         jogada.linha1()
     elif jogada.linha == 2:
@@ -82,11 +85,50 @@ while rounds != 0:
     elif jogada.linha == 3:
         jogada.linha3()
     
-    print()
-    print(jogada.jogo[0])
-    print(len(jogada.jogo[0]))
-    print()
+    # horizontal
+    conta = 0
+    for i in jogada.jogo[0]:
+        for j in i:
+            conta += 1
+            if conta == 3: 
+                print("Alguem ganha")
     
+    conta = 0
+    for i1 in jogada.jogo[1]:
+        for j1 in i1:
+            conta += 1
+            if conta == 3:
+                print("Alguem ganha")
+    
+    conta = 0
+    for i2 in jogada.jogo[2]:
+        for j2 in i2:
+            conta += 1
+            if conta == 3:
+                print("Alguem ganha") 
+    
+    
+    # diagonal
+    if jogada.token in jogada.jogo[0][0] and jogada.token in jogada.jogo[1][1] and jogada.token in jogada.jogo[2][2]:
+        print("Alguem ganha")      
+    if jogada.token in jogada.jogo[0][2] and jogada.token in jogada.jogo[1][1] and jogada.token in jogada.jogo[2][0]:
+        print("Alguem ganha")  
+    
+    # vertial
+    if jogada.token in jogada.jogo[0][0] and jogada.token in jogada.jogo[1][0] and jogada.token in jogada.jogo[2][0]:
+        print("Alguem ganha")      
+    if jogada.token in jogada.jogo[0][1] and jogada.token in jogada.jogo[1][1] and jogada.token in jogada.jogo[2][1]:
+        print("Alguem ganha") 
+    if jogada.token in jogada.jogo[0][2] and jogada.token in jogada.jogo[1][2] and jogada.token in jogada.jogo[2][2]:
+        print("Alguem ganha")      
+    
+
+ 
+
+
+
+
+    '''
     print("A jogar: Jogador 2")
     jogada.linha = int(input("Qual a linha em que queres jogar 1, 2, 3:"))
     if jogada.linha == 1:
@@ -95,4 +137,6 @@ while rounds != 0:
         jogada.linha2()
     elif jogada.linha == 3:
         jogada.linha3()
-print("empate")    
+print("empate")   
+    '''
+    
